92e1c5236f78b0a84cd84f71988fb5f49fcebe44
Peter Sutovsky <psregi@gmail.com>
2020-05-27 14:47:22 +0100

    Batch files and extracting from model file name extracted.

--- test/test_jsonOutput2Csv.py
+++ test/test_jsonOutput2Csv.py
@@ -6,7 +6,7 @@ import pandas as pd
 def test_add_data():
     colNames = ['a']
     inputDir = r'..\output\paramsAndREsults_20-05-08_00-14-30.json'
-    df = pd.DataFrame(columns=colNames)
+    df = pd.DataFrame(columns=['bnVersion', 'numTimeSteps', 'orderAutoregression', 'targetNode', 'outcomeID', 'accuracy', 'tp', 'fp', 'fn', 'tn', 'sensitivity', 'specificity', 'accuracy_calc', 'precision', 'prevalence', 'tpr', 'fpr', 'fnr', 'tnr', 'model_file_name'])
     df = jsonOutput2Csv.addData(df, inputDir)
     print()
     # >>> print p.search('no class at all')
